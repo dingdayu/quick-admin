f8f434febb585b006fd166a64e74dcd62c1a2eb0
dingdayu <614422099@qq.com>
2023-06-27 23:12:28 +0800

    feat: add less & add arco-design deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
 import Tov from './presets'
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-	plugins: [Tov()],
+  plugins: [Tov()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          hack: `true; @import (reference) "${resolve(
+            'src/assets/style/screen.less'
+          )}";`
+        },
+        javascriptEnabled: true
+      }
+    }
+  }
 })
